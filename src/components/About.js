f4577eb7f49d7cce75a6f93cf2e1139c35b5602d
Luke-Desktop <up858296@myport.ac.uk>
2020-06-16 13:27:12 +0100

    Added fade transition between pages

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function About() {
 
+    const [loading_animation, setloading_animation] = useState(false);
+
+
+    useEffect(() => {
+        if (loading_animation === false) {
+            setTimeout(() => {
+                setloading_animation(!loading_animation);
+                console.log('play animation')
+            }, 100);
+        }
+        return () => {
+            document.body.style.overflow = 'unset';
+        }
+
+    }, []);
+
     return(
-        <div>
+        <div className={loading_animation ? 'loading_transition fade' : 'loading_transition'}>
             <p>About page</p>
         </div>
     )
