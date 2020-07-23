c62b6e03e15d52ffef7ed24dbdc94dacc8d918f1
Luke-Desktop <up858296@myport.ac.uk>
2020-07-23 15:13:45 +0100

    added JSON data for portfolio items

---
+++ src/Data/Adobe_data.js
@@ -0,0 +1,47 @@
+//Adobe component data
+import MinimalMe from '../assets/Mylogo.svg';
+import SimplyfitPoster from '../assets/Simplyfitposter.jpg'
+import InsectInfographic from '../assets/insectMotiongraphic.png'
+
+export default[
+    {
+        'workName': 'Switch up your "Grub" Motiongraphic',
+        'workDescription': 'This is a motion graphic comparing the environmental and health effects of consuming animal products compared to insects as dietary source of protein.',
+        'workTech': ['Adobe After effects'],
+        'videoAddress': 'https://youtu.be/cloat51hzDY',
+        'imageAddress': ''
+    },
+
+    {
+        'workName': 'Simplyfit concept poster',
+        'workDescription': 'This is a poster developed explaining the concept of Simplyfit, a fitness application developed as part of my final year project.',
+        'workTech': ['Adobe Illustrator'],
+        'videoAddress': '',
+        'imageAddress': SimplyfitPoster,
+    },
+
+    {
+        'workName': 'Switch up your "Grub" Infographic',
+        'workDescription': 'This is an infographic developed explaining the benefits of consuming insects as a source of protein.',
+        'workTech': ['Adobe Illustrator'],
+        'videoAddress': '',
+        'imageAddress': InsectInfographic,
+    },
+
+    {
+        'workName': 'Crunchy Nut Advert spoof',
+        'workDescription': 'This video was made as a comedic spoof of a Crunchy Nut advert',
+        'workTech': ['Adobe Premier Pro'],
+        'videoAddress': 'https://youtu.be/y8S2RUYrLN8',
+        'imageAddress': '',
+    },
+
+    {
+        'workName': 'Icons and Designs',
+        'workDescription': 'These are a selection of logos and icons created by me.',
+        'workTech': ['Adobe Premier Pro'],
+        'videoAddress': '',
+        'imageAddress': MinimalMe,
+    },
+    
+]
