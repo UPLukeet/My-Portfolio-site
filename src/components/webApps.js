17581e4db385e46ac5dbabd9ec19d18e5fdad26c
Luke-Desktop <up858296@myport.ac.uk>
2020-09-02 11:45:49 +0100

    added working swipe libary and data cycling for the card layouts

--- src/components/webApps.js
+++ src/components/webApps.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Data from '../Data/webApps_data';
+import ReactSwing from '../../node_modules/react-swing/dist/react-swing.js';
 
 function Webapps() {
 
     const [loading_animation, setloading_animation] = useState(false);
     const [arrayChecker, set_arrayChecker] = useState(0);
+    const [arrayChecker2, set_arrayChecker2] = useState(1);
 
     useEffect(() => {
         if (loading_animation === false) {
@@ -13,29 +15,27 @@ function Webapps() {
                 console.log()
             }, 100);
         }
-
-        const script = document.createElement('script');
-        script.src = '@swipi-cards/dist/swipi-cards/swipi-cards.js';
-
-        script.async = true;
-
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        }
     }, [loading_animation]);
 
+    //cycles through data
     const arrayLimiter = () => {
         if (arrayChecker < (Data.length - 1)) {
             set_arrayChecker(arrayChecker + 1)
         } else {
             set_arrayChecker(0)
         }
-        console.log(Data);
+        if (arrayChecker2 < (Data.length - 1)) {
+            set_arrayChecker2(arrayChecker2 + 1)
+        } else {
+            set_arrayChecker2(0)
+        }
+        console.log(filteredData2)
     };
 
     const filteredData = Data[arrayChecker];
+    const filteredData2 = Data[arrayChecker2];
+
+
     const textTransition = loading_animation ? 'text_transition ease' : 'text_transition';
     const elementTransition = loading_animation ? 'element_transition ease' : 'element_transition';
 
@@ -43,17 +43,16 @@ function Webapps() {
 
     return (
         <div className='webAppStyles'>
-            <rg-swipi-cards stack-offset-y="0.3" class='hydrated'>
-                <rg-swipi-card left-color='green' right-color='green' class='hydrated'>
-                    <p>test1</p>
-                </rg-swipi-card>
-                <rg-swipi-card left-color='green' right-color='green' class='hydrated'>
-                    <p>test2</p>
-                </rg-swipi-card>
-            </rg-swipi-cards>
+            <ReactSwing
+                className="stack"
+            >
+                <div>{filteredData.workName}</div>
+
+                <div>{filteredData2.workName}</div>
+            </ReactSwing>
             <p>{filteredData.workName}</p>
             <button onClick={arrayLimiter}>Increase number</button>
-        </div>
+        </div >
     )
 }
 
